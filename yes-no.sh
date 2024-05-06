16109ff98b1c938682f3940041fc59cd18074198
Damian <147666866+BenGhostBear@users.noreply.github.com>
2024-05-06 13:45:06 -0700

    Create yes-no.sh

---
+++ yes-no.sh
@@ -0,0 +1,91 @@
+#! /bin/bash
+
+# ============================================================================
+# shell script to create command line lab structure
+# Source: STEAM Clown - www.steamclown.org 
+# GitHub: https://github.com/jimTheSTEAMClown/Linux
+# Hacker: Jim Burnham - STEAM Clown, Engineer, Maker, Propmaster & Adrenologist 
+# This example code is licensed under the CC BY-NC-SA 4.0, GNU GPL and EUPL
+# https://creativecommons.org/licenses/by-nc-sa/4.0/
+# https://www.gnu.org/licenses/gpl-3.0.en.html
+# https://eupl.eu/
+# Program/Design Name:		yes-no.sh
+# Description:    shell script to WGET the files to setup and update a cleanand shoiw asking a yes/No question 
+# Dependencies:   
+# Revision: 
+#  
+#  Revision 0.02 - Updated 03/18/2024 for SVCTE Mechatronics Class
+#  Revision 0.01 - Created 03/18/2024
+# Additional Comments:
+# ============================================================================
+                                                                                     
+# copy to your /home/<USER> directory
+# sudo wget -O ./yes-no.sh https://raw.githubusercontent.com/jimTheSTEAMClown/RaspberryPi/master/Yes-No.sh
+# sudo chmod 755 ./yes-no.sh
+# sudo cat ./yes-no.sh
+# ============================================================================
+echo " "
+echo "----------------------------------------------------"
+echo "Do you wish run the Ubuntu Class Image Update?"
+echo "----------------------------------------------------"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) 
+            echo "----------------------------------------------------"
+            echo "Running Ubuntu Class Image Update" 
+            echo "----------------------------------------------------"
+            break;;
+        No ) 
+            echo "----------------------------------------------------"
+            echo "Exiting Without Update" 
+            echo "----------------------------------------------------"
+            exit;;
+    esac
+done
+# ============================================================================
+echo " "
+echo "  _  _  ___  ___   __  ____  __  _  _  __ "
+echo " ( )( )(  ,\(   \ (  )(_  _)(  )( \( )/ _) "
+echo "  )()(  ) _/ ) ) )/__\  )(   )(  )  (( (/\ "
+echo "  \__/ (_)  (___/(_)(_)(__) (__)(_)\_)\__/ "
+echo "----------------------------------------------------"
+echo "Ready... I'm going to run "
+echo "  Going to run $ sudo apt update"
+echo "  Going to run $ sudo apt upgrade with -y" 
+echo "----------------------------------------------------"
+echo "Do you wish to run UPDATE and UPGRADE?  Enter y/Y or n/N or any Key?"
+read -p "update and upgrade?: " yesInstall
+# elif statements
+if [ $yesInstall == "y" ] || [ $yesInstall == "Y" ]
+then
+echo "----------------------------------------------------"
+echo "getting home with cd ~"
+echo "----------------------------------------------------"
+echo " "
+cd ~
+pwd
+echo "----------------------------------------------------"
+echo "Running $ update"
+echo "----------------------------------------------------"
+echo " "
+sudo apt update
+echo " "
+echo "----------------------------------------------------"
+echo "Done running Update"
+echo "----------------------------------------------------"
+echo "----------------------------------------------------"
+echo "Running $ upgrade with -y"
+echo "----------------------------------------------------"
+echo " "
+sudo apt upgrade -y
+echo " "
+echo "----------------------------------------------------"
+echo "Done running Upgrade"
+echo "----------------------------------------------------"
+elif [ $yesInstall == "n" ] || [ $yesInstall == "N" ]
+then
+echo "Skipping this install"
+else
+echo "Skipping this install"
+fi
+
